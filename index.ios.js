70f2a4a1841993db013c253a982e80fadc1f4768
davellaj <jamie.davella@gmail.com>
2017-05-22 01:10:17 -0400

    initialize react-native-udemy repo

---
+++ index.ios.js
@@ -0,0 +1,16 @@
+// Import a library to help create a Component
+import React from 'react';
+import { View, AppRegistry } from 'react-native';
+import Header from './src/components/Header';
+import AlbumList from './src/components/AlbumList';
+
+// Create a Component
+const App = () => (
+  <View>
+    <Header headerText={'Albums'} />
+    <AlbumList />
+  </View>
+);
+
+// Render it to the device
+AppRegistry.registerComponent('albums', () => App);
